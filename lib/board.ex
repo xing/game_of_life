aca4112ce36f2e3e592a16c6ada219f1a47a9dc3
Charles Bernasconi <charles.bernasconi@xing.com>
2016-11-07 12:21:52 +0100

    Added Board struct

--- lib/board.ex
+++ lib/board.ex
@@ -1,3 +1,6 @@
-defmodule GameOfLive.Board do
-  defstruct size: {0,0} 
+defmodule GameOfLife.Board do
+  defstruct(
+    size: {1,1}, # size of board {x,y}
+    alive_cells: [], # list of tuples of alive cells e.g. [{1,1}, {5,2}].  Bottom left is 0,0
+  )
 end
