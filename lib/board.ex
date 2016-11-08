fe6616926ce66cad6be465dfb056fff9b920daf6
nuria-bassols <nuria.bassols@xing.com>
2016-11-08 16:56:37 +0100

    Separate foreign_alive_cells in the Board

--- lib/board.ex
+++ lib/board.ex
@@ -3,68 +3,71 @@ defmodule GameOfLife.Board do
     generation: 0, # iteration number
     size: {5,5}, # size of board {x,y}
     alive_cells: %MapSet{}, # set of tuples of alive cells e.g. [{1,1}, {5,2}].  Bottom left is 0,0
+    foreign_alive_cells: %MapSet{} # set of tuples of alive cells
   )
 
   @neighbour_range -1..1
 
   def next_board_state(%GameOfLife.Board{} = board) do
-    new_alive_cells = MapSet.union(survivor_cells(board), newborn_cells(board))
-    %GameOfLife.Board{size: board.size, alive_cells: new_alive_cells, generation: board.generation + 1}
+    # TODO This is a chapu. Try to find a better way.
+    combined_board = %{board | alive_cells: MapSet.union(board.alive_cells, board.foreign_alive_cells)}
+    new_alive_cells = MapSet.union(survivor_cells(combined_board), newborn_cells(combined_board))
+    %{board | alive_cells: new_alive_cells, generation: board.generation + 1}
   end
 
-  def survivor_cells(%GameOfLife.Board{} = board) do
+  defp survivor_cells(%GameOfLife.Board{} = board) do
     board.alive_cells
     |> Enum.filter(&(within_board?(board.size, &1) and will_stay_alive?(board.alive_cells, &1)))
     |> MapSet.new
   end
 
-  def newborn_cells(%GameOfLife.Board{} = board) do
+  defp newborn_cells(%GameOfLife.Board{} = board) do
     board.alive_cells
-    |> Enum.flat_map(&(newborn_cells(board, &1)))
+    |> Enum.flat_map(&(newborn_neighbour_cells(board, &1)))
     |> Enum.filter(&(will_become_alive?(board.alive_cells, &1)))
     |> MapSet.new
   end
 
-  def newborn_cells(%GameOfLife.Board{} = board, alive_cell) do
+  defp newborn_neighbour_cells(%GameOfLife.Board{} = board, alive_cell) do
     neighbour_cells(alive_cell)
     |> Enum.filter(&(!alive_cell?(board, &1) and within_board?(board.size,&1)))
   end
 
-  def alive_cell?(%GameOfLife.Board{} = board, cell) do
+  defp alive_cell?(%GameOfLife.Board{} = board, cell) do
     Enum.member?(board.alive_cells, cell)
   end
 
-  def neighbour_ranges do
+  defp neighbour_ranges do
     @neighbour_range
     |> Enum.reduce([], fn(x,acc) -> acc ++ Enum.map(@neighbour_range, fn(y) -> {x,y} end) end)
     |> Enum.filter(&(&1 != {0,0}))
   end
 
-  def neighbour_cells({cell_x, cell_y}) do
+  defp neighbour_cells({cell_x, cell_y}) do
     neighbour_ranges()
     |> Enum.map(fn({x,y}) -> {cell_x + x, cell_y + y} end)
   end
 
-  def will_stay_alive?(board_cells, current_cell) do
+  defp will_stay_alive?(board_cells, current_cell) do
     alive_neighbours = count_alive_neighbours(board_cells, current_cell)
     alive_neighbours == 2 or alive_neighbours == 3
   end
 
-  def will_become_alive?(board_cells, current_cell) do
+  defp will_become_alive?(board_cells, current_cell) do
     alive_neighbours = count_alive_neighbours(board_cells, current_cell)
     alive_neighbours == 3
   end
 
-  def count_alive_neighbours(board_cells, current_cell) do
+  defp count_alive_neighbours(board_cells, current_cell) do
     Enum.count(board_cells, &(alive_neighbour?(current_cell, &1)))
   end
 
-  def alive_neighbour?({x_current,y_current} = current_cell, {x_candidate, y_candidate} = candidate) do
+  defp alive_neighbour?({x_current,y_current} = current_cell, {x_candidate, y_candidate} = candidate) do
     current_cell != candidate and
       abs(x_current - x_candidate) <= 1 and abs(y_current - y_candidate) <= 1
   end
 
-  def within_board?({size_x,size_y},{x,y}) do
+  defp within_board?({size_x,size_y},{x,y}) do
     x < size_x and y < size_y and x >= 0 and y >= 0
   end
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -3,120 +3,42 @@ defmodule BoardTest do
   doctest GameOfLife.Board
   import GameOfLife.Board
 
-  test "is within board" do
-    assert within_board?({4,4},{3,3})
-  end
-
-  test "is outside board" do
-    refute within_board?({4,4},{6,3})
-  end
-
-  test "is outside board on boundaries" do
-    refute within_board?({4,4},{4,0})
-    refute within_board?({4,4},{4,4})
-  end
-
-  test "is a neighbour" do
-    current = {5,5}
-    candidate = {6,6}
-    assert alive_neighbour?(current,candidate)
-  end
-
-  test "is also a neighbour" do
-    current = {5,5}
-    candidate = {4,5}
-    assert alive_neighbour?(current,candidate)
-  end
-
-  test "is not a neighbour" do
-    current = {5,5}
-    candidate = {6,7}
-    refute alive_neighbour?(current,candidate)
-  end
-
-  test "a cell is not a neighbour of himself" do
-    current = {5,5}
-    candidate = {5,5}
-    refute alive_neighbour?(current,candidate)
-  end
-
-  test "a cell does not have alive neighbours if board is dead" do
-    board_cells = []
-    current_cell = {5,5}
-    count = count_alive_neighbours(board_cells, current_cell)
-    assert 0 == count
-  end
-
-  test "a cell does not have alive neighbours" do
-    board_cells = [{2,2}]
-    current_cell = {5,5}
-    count = count_alive_neighbours(board_cells, current_cell)
-    assert 0 == count
-  end
-
-  test "a cell has alive neighbours" do
-    board_cells = [{4,5}, {6,6}]
-    current_cell = {5,5}
-    count = count_alive_neighbours(board_cells, current_cell)
-    assert 2 == count
-  end
-
-  test "a cell should stay alive if it has 2 neighbours" do
-    board_cells = [{4,5}, {6,6}]
-    current_cell = {5,5}
-    assert will_stay_alive?(board_cells, current_cell)
-  end
-
-  test "a cell should stay alive if it has 3 neighbours" do
-    board_cells = [{4,5}, {6,6}, {4,4}]
-    current_cell = {5,5}
-    assert will_stay_alive?(board_cells, current_cell)
-  end
-
-  test "a cell should not stay alive if it has >3 neighbours" do
-    board_cells = [{4,5}, {6,6}, {4,4}, {5,4}]
-    current_cell = {5,5}
-    refute will_stay_alive?(board_cells, current_cell)
-  end
-
-  test "a cell should not stay alive if it has <2 neighbours" do
-    board_cells = [{4,5}]
-    current_cell = {5,5}
-    refute will_stay_alive?(board_cells, current_cell)
-  end
-
-  test "get neighbours" do
-    cell = {5,5}
-    neighbour_cell = {6,6}
-    assert Enum.member?(neighbour_cells(cell), neighbour_cell)
-  end
-
-  test "get dead neighbours" do
-    board = %GameOfLife.Board{size: {5,5}, alive_cells: %MapSet{}}
-    dead_neighbour = [{0,0}]
-    assert dead_neighbour == newborn_cells(board,{-1,-1} )
-  end
-
-  test "get dead neighbours doesn't return alive cells" do
-    board = %GameOfLife.Board{size: {5,5}, alive_cells: MapSet.new [{0,0}]}
-    dead_neighbour = []
-    assert dead_neighbour == newborn_cells(board,{-1,-1} )
-  end
-
-  test "get newborn cells" do
-    board = %GameOfLife.Board{size: {5,5}, alive_cells: MapSet.new [{-1,-1}, {-1,0}, {0,-1}]}
-    assert MapSet.new([{0,0}]) == newborn_cells(board)
-  end
+  # test "a cell should stay alive if it has 2 neighbours" do
+  #   board_cells = [{4,5}, {6,6}]
+  #   current_cell = {5,5}
+  #   assert will_stay_alive?(board_cells, current_cell)
+  # end
+  #
+  # test "a cell should stay alive if it has 3 neighbours" do
+  #   board_cells = [{4,5}, {6,6}, {4,4}]
+  #   current_cell = {5,5}
+  #   assert will_stay_alive?(board_cells, current_cell)
+  # end
+  #
+  # test "a cell should not stay alive if it has >3 neighbours" do
+  #   board_cells = [{4,5}, {6,6}, {4,4}, {5,4}]
+  #   current_cell = {5,5}
+  #   refute will_stay_alive?(board_cells, current_cell)
+  # end
+  #
+  # test "a cell should not stay alive if it has <2 neighbours" do
+  #   board_cells = [{4,5}]
+  #   current_cell = {5,5}
+  #   refute will_stay_alive?(board_cells, current_cell)
+  # end
 
   test "get next state of board" do
-    board = %GameOfLife.Board{size: {5,5}, alive_cells: MapSet.new([{-1,-1}, {-1,0}, {0,-1}])}
-    new_board = %GameOfLife.Board{size: {5,5}, alive_cells: MapSet.new([{0,0}]), generation: 1}
-    assert next_board_state(board) == new_board
+    board = %GameOfLife.Board{size: {5,5}, foreign_alive_cells: MapSet.new([{-1,-1}, {-1,0}, {0,-1}])}
+    expected_board = %{board | alive_cells: MapSet.new([{0,0}]), generation: 1}
+    assert next_board_state(board) == expected_board
   end
 
   test "get next state of board 2" do
-    board = %GameOfLife.Board{size: {5,5}, alive_cells: MapSet.new [{-1,-1}, {-1,0}, {0,-1}, {0,0}, {-1,1}, {0,4}, {3,1}, {4,1}, {3,2}]}
-    new_board = %GameOfLife.Board{size: {5,5}, alive_cells: MapSet.new([{0,1}, {3,1}, {4,1}, {3,2}, {4,2}]), generation: 1}
+    board = %GameOfLife.Board{size: {5,5},
+      alive_cells: MapSet.new([{0,0}, {0,4}, {3,1}, {4,1}, {3,2}]),
+      foreign_alive_cells: MapSet.new([{-1,-1}, {-1,0}, {0,-1}, {-1,1}]) }
+
+    new_board = %{board | alive_cells: MapSet.new([{0,1}, {3,1}, {4,1}, {3,2}, {4,2}]), generation: 1}
     assert next_board_state(board) == new_board
   end
 
@@ -124,6 +46,11 @@ defmodule BoardTest do
     board = %GameOfLife.Board{generation: 5}
     new_board = %{board | generation: 6}
     assert next_board_state(board) == new_board
+  end
+
+  test "next board state keeps the foreign cells alive" do
+    board = %GameOfLife.Board{size: {5,5}, foreign_alive_cells: MapSet.new([{-1,-1}, {-1,0}, {0,-1}])}
+    assert board.foreign_alive_cells == next_board_state(board).foreign_alive_cells
   end
 
 end
