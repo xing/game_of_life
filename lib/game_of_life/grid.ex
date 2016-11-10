db3265497216bba738b5adf2e55d072d299dd826
GustavoCaso <gustavocaso@gmail.com>
2016-11-10 17:50:09 +0100

    Allow rows for the grid, and not compute update_board for the same board

--- lib/game_of_life/grid.ex
+++ lib/game_of_life/grid.ex
@@ -1,10 +1,18 @@
 defmodule GameOfLife.Grid do
+  @default_row_size 3
+
   defstruct board_size: nil, boards: [], board_server_pids: %{}
 
   def add_board(%__MODULE__{boards: []} = grid),
     do: {:ok, {0, 0}, %__MODULE__{grid | boards: [{0, 0}]}}
-  def add_board(%__MODULE__{board_size: {w, _h}, boards: [{x, y} | _]} = grid) do
-    board_id = {x + w, y}
+
+  def add_board(%__MODULE__{board_size: {w, h}, boards: boards} = grid) do
+    boards_count = length(boards)
+    row_id = rem(boards_count, @default_row_size)
+    column_id = trunc(div(boards_count, @default_row_size))
+
+    board_id = {row_id * w, column_id * h}
+
     {:ok, board_id, %__MODULE__{grid | boards: [board_id | grid.boards]}}
   end
 
