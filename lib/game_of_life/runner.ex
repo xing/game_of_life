f60a49c286042ca71ee2fcb5bccdb0cef1a83cd1
Charles Bernasconi <charles.bernasconi@xing.com>
2016-11-11 21:08:38 +0100

    Allow stopping and starting of runner

--- lib/game_of_life/runner.ex
+++ lib/game_of_life/runner.ex
@@ -2,10 +2,18 @@ defmodule GameOfLife.Runner do
   alias GameOfLife.{GridManager, Grid, Ticker, BoardServer}
 
   def run do
-     do_run
+    {:ok, %Ticker{interval: interval, ticker_state: ticker_state}} = Ticker.get_state
+    case ticker_state do
+      :started ->
+        run_next_turn()
+        :timer.sleep(interval)
+      :stopped ->
+        :timer.sleep(100) # Just wait and check the state again after a while
+    end
+    run
   end
 
-  def do_run do
+  def run_next_turn do
     {:ok, %Grid{board_server_pids: pids}} = GridManager.get_state
 
     Map.values(pids)
@@ -18,9 +26,5 @@ defmodule GameOfLife.Runner do
         {:ok, board } = BoardServer.current_board(pid)
         GenEvent.notify(GameOfLife.EventManager, {:board_update, board})
       end)
-
-    {:ok, %Ticker{interval: interval}} = Ticker.get_state
-    :timer.sleep(interval)
-    do_run
   end
 end
